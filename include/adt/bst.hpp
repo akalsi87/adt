4c56136dc590b4f9b3b24c987b4d5772cc31f822
Aaditya Kalsi <akalsi.dev@gmail.com>
2016-10-03 00:13:29 -0400

    Somewhat functional BST

--- include/adt/bst.hpp
+++ include/adt/bst.hpp
@@ -6,66 +6,302 @@
 #ifndef ADT_BST_HPP_INCL_GUARD
 #define ADT_BST_HPP_INCL_GUARD
 
+#include <cassert>
+#include <iostream>
 #include <functional>
+#include <utility>
 
 namespace adt
 {
+
+    template <class T>
+    class bst;
+
+    template<typename T>
+    std::ostream& operator<<(std::ostream&, const bst<T>&);
 
     namespace detail
     {
 
         template <class T>
-        struct bst_node
+        class bst_node
         {
-            bst_node<T>* left;
-            bst_node<T>* right;
+          public:
+            bst_node* left;
+            bst_node* right;
+            bst_node* parent;
             T val;
+
+            bst_node(T&& v) : left(nullptr), right(nullptr), parent(nullptr), val(v) { }
+            ~bst_node() { }
+          private:
+            bst_node(const bst_node&) = delete;
+            bst_node& operator=(const bst_node&) = delete;
         };
 
         template <class T>
         class bst_iterator
         {
-          public:
-            bst_iterator(bst_node<T>* root);
-
-            ~bst_iterator();
-
-            bst_iterator(const bst_iterator& rhs);
-
-            bst_iterator& operator++();
-
-            bst_iterator& operator=(const bst_iterator& rhs);
-
           private:
-            bst_node<T>* root_;
-            bst_node<T>* parent_;
-            bst_node<T>* curr_;
+            using node = bst_node<T>;
+          public:
+            bst_iterator(node* curr)
+              : curr_(curr)
+            { }
+
+            bst_iterator(const bst_iterator& rhs) = default;
+
+            bst_iterator& operator++()
+            {
+                node* curr = curr_;
+                if (curr->right) {
+                    curr = curr->right;
+                    if (curr) {
+                        while (curr->left) { curr = curr->left; }
+                    }
+                    curr_ = curr;
+                } else {
+                    node* parent = curr->parent;
+                    if (parent) {
+                        while (curr == parent->right) {
+                            curr = parent;
+                            parent = curr->parent;
+                            if (!parent) {
+                                break;
+                            }
+                        }
+                        curr_ = parent;
+                    } else {
+                        curr = curr->right;
+                        if (curr) {
+                            while (curr->left) { curr = curr->left; }
+                        }
+                        curr_ = curr;
+                    }
+                }
+                return *this;
+            }
+
+            bst_iterator operator++(int)
+            {
+                bst_iterator it = *this;
+                ++(*this);
+                return it;
+            }
+
+            bst_iterator& operator--()
+            {
+                return *this;
+            }
+
+            bst_iterator operator--(int)
+            {
+                bst_iterator it = *this;
+                --(*this);
+                return it;
+            }
+
+            T& operator*() { return curr_->val; }
+
+            const T& operator*() const { return curr_->val; }
+
+            bst_iterator& operator=(const bst_iterator& rhs) = default;
+
+            bool operator==(const bst_iterator& rhs) const
+            {
+                return curr_ == rhs.curr_;
+            }
+
+            bool operator!=(const bst_iterator& rhs) const
+            {
+                return !(*this == rhs);
+            }
+          private:
+            node* curr_;
+
+            friend class bst<T>;
         };
 
     } // namespace detail
 
-    template <class T, class Comp = std::less<T>>
+    template <class T>
     class bst
     {
       public:
-        using iterator = T*;
+        using iterator = detail::bst_iterator<T>;
 
-        bst();
+        bst() : root_(nullptr), leftmost_(nullptr) { }
 
-        ~bst();
+        ~bst() { clear(); }
 
-        iterator find(const T& val) const;
+        iterator find(const T& val) const
+        {
+            node* curr = root_;
+            while (curr != nullptr) {
+                if (curr->val == val) {
+                    // found it!
+                    break;
+                }
+                if (curr->val > val) {
+                    // search left
+                    curr = curr->left;
+                } else {
+                    // search right
+                    curr = curr->right;
+                }
+            }
+            return curr ? iterator(curr) : end();
+        }
 
-        std::pair<iterator, bool> insert(const T& val);
+        std::pair<iterator, bool> insert(const T& val)
+        {
+            return insert(T(val));
+        }
 
-        std::pair<iterator, bool> insert(T&& val);
+        std::pair<iterator, bool> insert(T&& val)
+        {
+            node* prev = nullptr;
+            node* curr = root_;
+            bool was_smaller = true;
+            bool was_always_smaller = true;
+            while (curr != nullptr) {
+                if (curr->val == val) {
+                    // found
+                    break;
+                }
+                prev = curr;
+                if (curr->val > val) {
+                    // search left
+                    curr = curr->left;
+                    was_smaller = true;
+                } else {
+                    // search right
+                    curr = curr->right;
+                    was_smaller = false;
+                    was_always_smaller = false;
+                }
+            }
+            if (curr) {
+                return std::make_pair(iterator(curr), false);
+            } else {
+                node* new_node = create(std::forward<T>(val));
+                new_node->parent = prev;
+                if (prev) {
+                    if (was_smaller) {
+                        prev->left = new_node;
+                    } else {
+                        prev->right = new_node;
+                    }
+                } else {
+                    root_ = new_node;
+                }
+                if (was_smaller && was_always_smaller) {
+                    leftmost_ = new_node;
+                }
+                return std::make_pair(iterator(new_node), true);
+            }
+        }
 
-        size_t erase(const T& val);
+        size_t erase(const T& val)
+        {
+            auto it = find(val);
+            if (it != end()) {
+                erase(it);
+                return 1;
+            } else {
+                return 0;
+            }
+        }
 
-        iterator erase(iterator it);
+        iterator erase(iterator it)
+        {
+            node* curr = it.curr_;
+            assert(curr);
+            leftmost_ = (curr == leftmost_) ? curr->parent : leftmost_;
+            if (curr->left == nullptr && curr->right == nullptr) {
+                /* no children case */
+                iterator next = ++it;
+                if (node* parent = curr->parent) {
+                    if (curr == parent->left) {
+                        parent->left = nullptr;
+                    } else {
+                        parent->right = nullptr;
+                    }
+                } else {
+                    root_ = nullptr;
+                }
+                dispose(curr);
+                return next;
+            } else {
+                return end();
+            }
+        }
+
+        iterator begin()
+        {
+            return iterator(leftmost_);
+        }
+
+        iterator end() const
+        {
+            return iterator(nullptr);
+        }
+
+        void clear()
+        {
+            destroy_tree(root_);
+            root_ = leftmost_ = nullptr;
+        }
       private:
-        bst_node<T>* root_;
+        using node = detail::bst_node<T>;
+        node* root_;
+        node* leftmost_;
+
+        // don't allow copies yet
+        bst(const bst&) = delete;
+        bst& operator=(const bst&) = delete;
+
+        static node* create(T&& val)
+        {
+            return new node(std::forward<T>(val));
+        }
+
+        static void dispose(node* n)
+        {
+            delete n;
+        }
+
+        static void destroy_tree(node* n)
+        {
+            if (!n) { return; }
+            node* lft = n->left;
+            node* rgt = n->right;
+            dispose(n);
+            destroy_tree(lft);
+            destroy_tree(rgt);
+        }
+
+        static void print_tree(node* n, std::ostream& os)
+        {
+            if (!n) { return; }
+            iterator it(n);
+            const iterator end(nullptr);
+            while (it != end) {
+                os << " " << *it++ << " ";
+            }
+        }
+
+        friend std::ostream& operator<< <> (std::ostream& os, const bst& t);
     };
+
+    template <class T>
+    std::ostream& operator<<(std::ostream& os, const bst<T>& t)
+    {
+        os << "{";
+        t.print_tree(t.leftmost_, os);
+        os << "}";
+        return os;
+    }
 
 } // namespace adt
 
--- test/test_bst.cpp
+++ test/test_bst.cpp
@@ -3,8 +3,56 @@
  * \date Oct 02, 2016
  */
 
+#include <vector>
+#include <algorithm>
 #include "unittest.hpp"
 
-CPP_TEST( no_op )
+#include "adt/bst.hpp"
+
+CPP_TEST( bstInt )
 {
+    adt::bst<int> intSet;
+    {// test empty
+        TEST_TRUE(intSet.find(1) == intSet.end());
+        std::cout << "Empty set: " << intSet << std::endl;
+    }
+    {// insert single element
+        auto itInsPair = intSet.insert(5);
+        TEST_TRUE(*(itInsPair.first) == 5);
+        TEST_TRUE(itInsPair.second);
+        TEST_TRUE(*intSet.begin() == 5);
+        TEST_TRUE(intSet.find(5) != intSet.end());
+        TEST_TRUE(*intSet.find(5) == 5);
+        std::cout << "Set with 5: " << intSet << std::endl;
+        TEST_TRUE(intSet.erase(5) == 1);
+        std::cout << "Empty set: " << intSet << std::endl;
+        itInsPair = intSet.insert(5);
+        TEST_TRUE(*(itInsPair.first) == 5);
+        TEST_TRUE(itInsPair.second);
+        TEST_TRUE(*intSet.begin() == 5);
+        TEST_TRUE(intSet.find(5) != intSet.end());
+        TEST_TRUE(*intSet.find(5) == 5);
+        TEST_TRUE(intSet.erase(intSet.find(5)) == intSet.end());
+    }
+    {// insert 10 elements
+        std::vector<int> values;
+        for (int i = 0; i < 10; ++i) {
+            values.push_back(i);
+        }
+        std::random_shuffle(values.begin(), values.end());
+
+        for (int i : values) {
+            auto r = intSet.insert(i);
+            TEST_TRUE(*(r.first) == i);
+            TEST_TRUE(r.second);
+            std::cout << intSet << std::endl;
+        }
+
+        int count = 0;
+        for (int i : intSet) {
+            TEST_TRUE(i == count++);
+        }
+        intSet.clear();
+        TEST_TRUE(intSet.begin() == intSet.end());
+    }
 }
